44bd364bf936dd32934424ba7cc4a8e86bf78203
markadut <mark_adut@brown.edu>
2021-12-02 03:23:44 +0000

    pivottable

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
 import dash
+from dash import dash_table
 from dash import dcc # dash core components
 from dash import html
+import dash_pivottable
+import pandas as pd
+
+df = pd.read_csv('https://bit.ly/elements-periodic-table')
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div(children=[
-    html.H2(children='Greetings!'),
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [5, 7, 2], 'type': 'line'},
-            ],
-            'layout': {
-                'title': 'Example Graph'
-            }
-        }
-    )
-])
+app.layout = dash_table.DataTable(
+    id='table',
+    columns=[{"name": i, "id": i} for i in df.columns],
+    data=df.to_dict('records'),
+)
 
-app.run_server(debug=True, host="0.0.0.0")
+app.layout = html.Div(
+    dash_pivottable.PivotTable(
+        data=[
+            ["AtomicNumber","Element","Symbol","AtomicMass"],
+            [1,"Hydrogen","H",1.007],
+            [2,"Helium","He",4.002],
+            [3,"Lithium","Li",6.941],
+            [4,"Beryllium","Be",9.012],
+        ],
+        cols=["AtomicNumber"],
+        rows=["Element"],
+        vals=["Count"]
+    )
+)
+
