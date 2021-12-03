c83f9eff40231fcef4e1ded7596d856aca50a6bf
markadut <mark_adut@brown.edu>
2021-12-03 23:47:26 +0000

    Final pivottable using dash-pivottable

--- app.py
+++ app.py
@@ -2,33 +2,63 @@ import dash
 from dash import dash_table
 from dash import dcc # dash core components
 from dash import html
+from dash.dependencies import Input, Output
 import dash_pivottable
+
 import pandas as pd
 
 df = pd.read_csv('https://bit.ly/elements-periodic-table')
 
 app = dash.Dash(__name__)
+def identity(x): return x
 
-app.layout = dash_table.DataTable(
-    id='table',
-    columns=[{"name": i, "id": i} for i in df.columns],
-    data=df.to_dict('records'),
+cols = df.columns
+
+app.layout = html.Div(children=[
+    html.H2(children='Periodic Pivot Table (Cool!)'),
+    dcc.Dropdown(
+        id="index_dropdown",
+        options=[{'label': i, 'value': i} for i in cols],
+        multi=False,
+        placeholder='Please select a index!'
+    ),
+    dcc.Dropdown(
+        id="column_dropdown",
+        options=[{'label': col, 'value': col} for col in cols],
+        multi=False,
+        placeholder='Please select a column!'
+    ),
+    dcc.Dropdown(
+        id="value_dropdown",
+        options=[{'label': val, 'value': val} for val in cols],
+        multi=False,
+        placeholder='Please select a value!'
+    ),
+    html.Div(id='output-data-upload'),
+])
+
+@app.callback(
+    Output('output-data-upload', 'children'),
+    [Input('index_dropdown', 'value'), 
+     Input('column_dropdown', 'value'), 
+     Input('value_dropdown', 'value')]
 )
+def update_output(i, c, v):
+    if i is not None and c is not None and v is not None:
+        # pt = df.pivot_table(index=i, columns=c, values=v, aggfunc=identity)
+        print(i)
+        print(c)
+        print(v)
+        rows = [list(df.iloc[[i]]) for i in range(df.shape[0])]
+        rows.insert (0, df.columns) 
 
-app.layout = html.Div(
-    dash_pivottable.PivotTable(
-        data=[
-            ["AtomicNumber","Element","Symbol","AtomicMass"],
-            [1,"Hydrogen","H",1.007],
-            [2,"Helium","He",4.002],
-            [3,"Lithium","Li",6.941],
-            [4,"Beryllium","Be",9.012],
-        ],
-
-        cols=["AtomicNumber"],
-        rows=["Element"],
-        vals=["Count"]
+        return html.Div(
+            dash_pivottable.PivotTable(
+                data=rows,
+                cols=["Element"],
+                rows=[i],
+                vals=[]
+            )
     )
-)
 
 app.run_server(debug=True, host="0.0.0.0")
