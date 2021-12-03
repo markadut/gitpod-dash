62e353a8e0fa050b7892c476f93b951a9d24bfbf
markadut <mark_adut@brown.edu>
2021-12-03 23:49:53 +0000

    pivottable_adut

--- app.py
+++ app.py
@@ -5,14 +5,19 @@ from dash import html
 from dash.dependencies import Input, Output
 import dash_pivottable
 
+
 import pandas as pd
 
+
 df = pd.read_csv('https://bit.ly/elements-periodic-table')
+
 
 app = dash.Dash(__name__)
 def identity(x): return x
 
+
 cols = df.columns
+
 
 app.layout = html.Div(children=[
     html.H2(children='Periodic Pivot Table (Cool!)'),
@@ -37,28 +42,31 @@ app.layout = html.Div(children=[
     html.Div(id='output-data-upload'),
 ])
 
+
 @app.callback(
     Output('output-data-upload', 'children'),
     [Input('index_dropdown', 'value'), 
      Input('column_dropdown', 'value'), 
      Input('value_dropdown', 'value')]
 )
-def update_output(i, c, v):
-    if i is not None and c is not None and v is not None:
-        # pt = df.pivot_table(index=i, columns=c, values=v, aggfunc=identity)
-        print(i)
-        print(c)
-        print(v)
-        rows = [list(df.iloc[[i]]) for i in range(df.shape[0])]
+def update_output(index, column, value):
+    if index is not None and column is not None and values is not None:
+        # pt = df.pivot_table(index=index, columns=column, values=value, aggfunc=identity)
+        print(index)
+        print(column)
+        print(value)
+
+        rows = [list(df.iloc[[index]]) for index in range(df.shape[0])]
         rows.insert (0, df.columns) 
 
         return html.Div(
             dash_pivottable.PivotTable(
                 data=rows,
                 cols=["Element"],
-                rows=[i],
+                rows=[index],
                 vals=[]
             )
     )
+
 
 app.run_server(debug=True, host="0.0.0.0")
